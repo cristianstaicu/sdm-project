56fbca43901e52f2ea285170878124b85f63130b
rickyyy <r.bortolameotti@gmail.com>
2013-11-19 13:03:21 +0100

    change db script

--- 1stProject/script/Dump20131112.sql
+++ 1stProject/script/Dump20131112.sql
@@ -1,249 +1,238 @@
-CREATE SCHEMA sdm_ac;
-USE  sdm_ac ;
-DROP TABLE IF EXISTS sdm_ac.company ;
+CREATE DATABASE  IF NOT EXISTS `sdm_ac` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `sdm_ac`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: localhost    Database: sdm_ac
+-- ------------------------------------------------------
+-- Server version	5.6.14-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  sdm_ac.company  (
-   id  int(11) NOT NULL,
-   name  varchar(45) NOT NULL,
-   location  varchar(45) NOT NULL,
-   contact  varchar(45) NOT NULL,
-  PRIMARY KEY ( id ),
-  UNIQUE KEY  id_UNIQUE  ( id )
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `location` varchar(45) NOT NULL,
+  `contact` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table  company 
+-- Dumping data for table `company`
 --
 
-LOCK TABLES  sdm_ac.company  WRITE;
-/*!40000 ALTER TABLE  company  DISABLE KEYS */;
-/*!40000 ALTER TABLE  company  ENABLE KEYS */;
+LOCK TABLES `company` WRITE;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table  doctor 
+-- Table structure for table `doctor`
 --
 
-DROP TABLE IF EXISTS  sdm_ac.doctor ;
+DROP TABLE IF EXISTS `doctor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  sdm_ac.doctor  (
-   id  int(11) NOT NULL,
-   name  varchar(45) NOT NULL,
-   surname  varchar(45) NOT NULL,
-   department  varchar(45) NOT NULL,
-   id_hospital  varchar(45) DEFAULT NULL,
-   id_healthclub  varchar(45) DEFAULT NULL,
-  PRIMARY KEY ( id ),
-  UNIQUE KEY  id_UNIQUE  ( id )
+CREATE TABLE `doctor` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `surname` varchar(45) NOT NULL,
+  `department` varchar(45) NOT NULL,
+  `id_hospital` varchar(45) DEFAULT NULL,
+  `id_healthclub` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table  doctor 
+-- Dumping data for table `doctor`
 --
 
-LOCK TABLES  sdm_ac.doctor  WRITE;
-/*!40000 ALTER TABLE  doctor  DISABLE KEYS */;
-/*!40000 ALTER TABLE  doctor  ENABLE KEYS */;
+LOCK TABLES `doctor` WRITE;
+/*!40000 ALTER TABLE `doctor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `doctor` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table  employer 
+-- Table structure for table `employer`
 --
 
-DROP TABLE IF EXISTS  sdm_ac.employer ;
+DROP TABLE IF EXISTS `employer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  sdm_ac.employer  (
-   id  int(11) NOT NULL,
-   name  varchar(45) NOT NULL,
-   surname  varchar(45) NOT NULL,
-   id_company  int(11) NOT NULL,
-  PRIMARY KEY ( id ),
-  UNIQUE KEY  id_UNIQUE  ( id )
+CREATE TABLE `employer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `surname` varchar(45) NOT NULL,
+  `id_company` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table  employer 
+-- Dumping data for table `employer`
 --
 
-LOCK TABLES  sdm_ac.employer  WRITE;
-/*!40000 ALTER TABLE  employer  DISABLE KEYS */;
-/*!40000 ALTER TABLE  employer  ENABLE KEYS */;
+LOCK TABLES `employer` WRITE;
+/*!40000 ALTER TABLE `employer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employer` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table  health_club 
+-- Table structure for table `health_club`
 --
 
-DROP TABLE IF EXISTS  sdm_ac.health_club ;
+DROP TABLE IF EXISTS `health_club`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  sdm_ac.health_club  (
-   id  int(11) NOT NULL,
-   name  varchar(45) NOT NULL,
-   location  varchar(45) NOT NULL,
-   contact  varchar(45) NOT NULL,
-  PRIMARY KEY ( id ),
-  UNIQUE KEY  id_UNIQUE  ( id )
+CREATE TABLE `health_club` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `location` varchar(45) NOT NULL,
+  `contact` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table  health_club 
+-- Dumping data for table `health_club`
 --
 
-LOCK TABLES  sdm_ac.health_club  WRITE;
-/*!40000 ALTER TABLE  health_club  DISABLE KEYS */;
-/*!40000 ALTER TABLE  health_club  ENABLE KEYS */;
+LOCK TABLES `health_club` WRITE;
+/*!40000 ALTER TABLE `health_club` DISABLE KEYS */;
+/*!40000 ALTER TABLE `health_club` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table  hospital 
+-- Table structure for table `health_data`
 --
 
-DROP TABLE IF EXISTS  sdm_ac.hospital ;
+DROP TABLE IF EXISTS `health_data`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  sdm_ac.hospital  (
-   id  int(11) NOT NULL,
-   name  varchar(45) NOT NULL,
-   location  varchar(45) NOT NULL,
-   contact  varchar(45) NOT NULL,
-  PRIMARY KEY ( id ),
-  UNIQUE KEY  id_UNIQUE  ( id )
+CREATE TABLE `health_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_patient` int(11) NOT NULL,
+  `id_hospital` int(11) DEFAULT NULL,
+  `id_doctor` int(11) DEFAULT NULL,
+  `id_healthclub` int(11) DEFAULT NULL,
+  `value` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `measurement_type` varchar(255) NOT NULL,
+  `statement` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table  hospital 
+-- Dumping data for table `health_data`
 --
 
-LOCK TABLES  sdm_ac.hospital  WRITE;
-/*!40000 ALTER TABLE  hospital  DISABLE KEYS */;
-/*!40000 ALTER TABLE  hospital  ENABLE KEYS */;
+LOCK TABLES `health_data` WRITE;
+/*!40000 ALTER TABLE `health_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `health_data` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table  insurance 
+-- Table structure for table `hospital`
 --
 
-DROP TABLE IF EXISTS  sdm_ac.insurance ;
+DROP TABLE IF EXISTS `hospital`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  sdm_ac.insurance  (
-   id  int(11) NOT NULL,
-   name  varchar(45) NOT NULL,
-   location  varchar(45) NOT NULL,
-   contact  varchar(45) NOT NULL,
-  PRIMARY KEY ( id ),
-  UNIQUE KEY  id_UNIQUE  ( id )
+CREATE TABLE `hospital` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `location` varchar(45) NOT NULL,
+  `contact` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table  insurance 
+-- Dumping data for table `hospital`
 --
 
-LOCK TABLES  sdm_ac.insurance  WRITE;
-/*!40000 ALTER TABLE  insurance  DISABLE KEYS */;
-/*!40000 ALTER TABLE  insurance  ENABLE KEYS */;
+LOCK TABLES `hospital` WRITE;
+/*!40000 ALTER TABLE `hospital` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hospital` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table  patient_health_data 
+-- Table structure for table `insurance`
 --
 
-DROP TABLE IF EXISTS  sdm_ac.patient_health_data ;
+DROP TABLE IF EXISTS `insurance`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  sdm_ac.patient_health_data  (
-   id  int(11) NOT NULL,
-   id_patient  int(11) NOT NULL,
-   id_hospital  int(11) NOT NULL,
-   id_doctor  int(11) NOT NULL,
-   disease  varchar(255) NOT NULL,
-   surgery  binary(1) NOT NULL,
-   date  date NOT NULL,
-   treatment  varchar(255) NOT NULL,
-   drug_prescription  varchar(255) NOT NULL,
-  PRIMARY KEY ( id ),
-  UNIQUE KEY  id_UNIQUE  ( id )
+CREATE TABLE `insurance` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `location` varchar(45) NOT NULL,
+  `contact` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table  patient_health_data 
+-- Dumping data for table `insurance`
 --
 
-LOCK TABLES  sdm_ac.patient_health_data  WRITE;
-/*!40000 ALTER TABLE  patient_health_data  DISABLE KEYS */;
-/*!40000 ALTER TABLE  patient_health_data  ENABLE KEYS */;
+LOCK TABLES `insurance` WRITE;
+/*!40000 ALTER TABLE `insurance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `insurance` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table  personal_health_data 
+-- Table structure for table `patient`
 --
 
-DROP TABLE IF EXISTS  sdm_ac.personal_health_data ;
+DROP TABLE IF EXISTS `patient`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  sdm_ac.personal_health_data  (
-   id  int(11) NOT NULL,
-   name  varchar(45) NOT NULL,
-   surname  varchar(45) NOT NULL,
-   birthdate  date NOT NULL,
-   gender  varchar(1) NOT NULL,
-   address  varchar(45) NOT NULL,
-   id_doctor  int(11) NOT NULL,
-   id_employer  int(11) DEFAULT NULL,
-   id_insurance  int(11) NOT NULL,
-  PRIMARY KEY ( id ),
-  UNIQUE KEY  id_UNIQUE  ( id )
+CREATE TABLE `patient` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `surname` varchar(45) NOT NULL,
+  `birthday` date NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `id_doc` varchar(45) DEFAULT NULL,
+  `id_emp` varchar(45) DEFAULT NULL,
+  `id_ins` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table  personal_health_data 
+-- Dumping data for table `patient`
 --
 
-LOCK TABLES  sdm_ac.personal_health_data  WRITE;
-/*!40000 ALTER TABLE  personal_health_data  DISABLE KEYS */;
-/*!40000 ALTER TABLE  personal_health_data  ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table  training_related_health_data 
---
-
-DROP TABLE IF EXISTS  sdm_ac.training_related_health_data ;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  sdm_ac.training_related_health_data  (
-   id  int(11) NOT NULL,
-   id_patient  varchar(45) NOT NULL,
-   id_health_club  varchar(45) NOT NULL,
-   disease  varchar(45) NOT NULL,
-   surgery  binary(1) NOT NULL,
-   training  varchar(255) NOT NULL,
-   period_start  date NOT NULL,
-   period_end  date NOT NULL,
-  PRIMARY KEY ( id ),
-  UNIQUE KEY  id_UNIQUE  ( id )
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table  training_related_health_data 
---
-
-LOCK TABLES  sdm_ac.training_related_health_data  WRITE;
-/*!40000 ALTER TABLE  training_related_health_data  DISABLE KEYS */;
-/*!40000 ALTER TABLE  training_related_health_data  ENABLE KEYS */;
+LOCK TABLES `patient` WRITE;
+/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -255,4 +244,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-11-12 10:55:33
+-- Dump completed on 2013-11-19 13:02:26
